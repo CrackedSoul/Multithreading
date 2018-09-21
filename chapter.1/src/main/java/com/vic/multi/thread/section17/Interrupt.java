05cd3662dde23b9e9cfbfad4eb9910319def27ae
CrackedSoul <1410914605@qq.com>
2018-09-21 17:33:25 +0800

    20180921

--- chapter.1/src/main/java/com/vic/multi/thread/section17/Interrupt.java
+++ chapter.1/src/main/java/com/vic/multi/thread/section17/Interrupt.java
@@ -9,14 +9,16 @@ public class Interrupt {
         try {
             Thread t1 = new Thread1();
             Thread t2 = new Thread(t1);
-            t2.start();
+//            t2.start();
             Thread.sleep(500);
-            t2.interrupt();
-//            t1.start();
-            Thread.sleep(500);
-//            t1.interrupt();
+//            t2.interrupt();
+            t1.interrupt();
             System.out.println("t1.isInterrupted()=" + t1.isInterrupted());
-            System.out.println("t2.isInterrupted()=" + t2.isInterrupted());
+            t1.start();
+            Thread.sleep(500);
+            t1.interrupt();
+            System.out.println("t1.isInterrupted()=" + t1.isInterrupted());
+//            System.out.println("t2.isInterrupted()=" + t2.isInterrupted());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -33,6 +35,9 @@ class Thread1 extends Thread {
 
         }
         System.out.println("interrupted");
+        while(true){}
+
+
 //        return;
     }
 }
